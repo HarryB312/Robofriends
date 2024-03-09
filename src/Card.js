35347ccb3b152df632d0b89435b9fbb5a5e755e4
harrison boswell <harryb@harrisons-MBP.lan>
2024-03-08 21:03:04 -0500

    Updated Cards

--- src/Card.js
+++ src/Card.js
@@ -1,16 +1,15 @@
-import React from "react"
+import React from "react";
 
-const Card = () => {
+const Card = (props) => {
   return(
     <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-      <img src="https://robohash.org/test" alt="" />
-      <h2>Jane Doe</h2>
-      <p>jane.doe@gmail.com</p>
+      <img src={`https://robohash.org/${props.id}`} alt="" />
+      <div>
+        <h2>{props.name}</h2>
+        <p>{props.email}</p>
       </div>
+    </div>
   )
 }
 
-export default Card
-
-
-    
+export default Card     
